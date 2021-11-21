9967fd1a01cd4a608c9fee5c2ab01d82d9f3bfee
Luiz Miguel dos Santos Gonçalves <lm4academy@gmail.com>
2021-11-21 12:37:37 -0300

    menu scroll added

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 import pygame
 import typing
 
+from pygame import surface
+
 
 pygame.init()
 pygame.font.init()
@@ -22,11 +24,14 @@ class Menu:
     font_color = (0, 0, 0)
     bg_hid_color = (255, 255, 255)
 
-    def __init__(self, pos):    
+    def __init__(self, pos, dim: tuple): 
+        self.image: pygame.Surface
         self.surface: pygame.Surface
         self.rect: pygame.Rect
+        self.relative_rect: pygame.Rect
         self.pos = pos
         self.changes = False
+        self.dim = dim
 
         options = ['line', 'line', "ola", "hei", 'delo', 'i']
         self.options: typing.List[MenuItem] = list()
@@ -35,35 +40,53 @@ class Menu:
             mi = MenuItem()
             mi.text = option
             mi.text_rendered = self.font.render(option, True, self.font_color)
+            mi.selected = False
             self.options.append(mi)
         
         self.plot()
     
-    def select(self, option_sel):
+    def scroll(self, pos, down=True):
+        if down:
+            if self.relative_rect.bottom > self.rect.h:
+                self.relative_rect.move_ip(0, -7)
+            if self.relative_rect.bottom < self.rect.h:
+                self.relative_rect.bottom = self.rect.h
+        else:
+            if self.relative_rect.y < 0:
+                self.relative_rect.move_ip(0, 7)
+            if self.relative_rect.y > 0:
+                self.relative_rect.y = 0
+        
+        self.mouse_movement(pos)
+        
         self.changes = True
-        self.surface.fill((0, 0, 0, 0))
+        self.draw_image()
 
-        for option in self.options:
-            option.selected = False
-            if option is option_sel:
-                pygame.draw.rect(self.surface, (0, 0, 0, 100), option.rect)
-                option.selected = True
-            self.surface.blit(option.image, option.rect)
     
     def draw_image(self):
         if not self.changes:
             return
         self.surface.fill((0, 0, 0, 0))
+        self.image.fill((0, 0, 0, 0))
+        
         for option in self.options:
-            self.surface.blit(option.image, (option.rect.x, option.rect.y))
+            if option.selected:
+                pygame.draw.rect(self.surface, (0, 0, 0, 100), option.rect)
+            self.surface.blit(option.image, option.rect)
+        
+        self.image.blit(self.surface, self.relative_rect)
+
         self.changes = False
     
     def mouse_movement(self, pos):
-        relative_pos = pygame.math.Vector2(pos) - pygame.math.Vector2(self.pos)
+        relative_pos = pygame.math.Vector2(pos) - pygame.math.Vector2(self.pos) - pygame.math.Vector2(self.relative_rect.x, self.relative_rect.y)
         for option in self.options:
             if option.rect.collidepoint(relative_pos):
-                self.select(option)
-                break
+                option.selected = True
+            else:
+                option.selected = False
+        self.changes = True
+        self.draw_image()
 
     def plot(self):
         max_x = max([x.text_rendered.get_width() for x in self.options])+2*self.padding
@@ -77,8 +100,15 @@ class Menu:
             current_y += option.image.get_height()
 
         self.surface = pygame.Surface((max_x, current_y), flags=pygame.SRCALPHA)
-        self.rect = self.surface.get_rect()
-        self.rect = self.rect.move(self.pos)
+        self.relative_rect = self.surface.get_rect()
+
+        if self.dim[0] is None:
+            self.image = pygame.Surface((max_x, self.dim[1]), flags=pygame.SRCALPHA)
+        else:
+            self.image = pygame.Surface(self.dim, flags=pygame.SRCALPHA)
+        
+        self.rect = self.image.get_rect()
+        self.rect.x, self.rect.y = self.pos
 
         self.changes = True
 
@@ -88,7 +118,7 @@ class Menu:
 class AppManager:
     def __init__(self, width, heigth) -> None:
         self.screen = pygame.display.set_mode((width, heigth))
-        self.menu = Menu((0, 0))
+        self.menu = Menu((0, 0), (None, 200))
         self.canva = Canva()
     
     def run(self):
@@ -97,19 +127,26 @@ class AppManager:
 
         while running:
             self.screen.fill(pygame.Color('white'))
-            self.screen.blit(self.menu.surface, self.menu.pos)
+            self.screen.blit(self.menu.image, self.menu.pos)
             pygame.display.update()
 
             for event in pygame.event.get():
                 if event.type == pygame.QUIT:
                     running = False
+                if event.type == pygame.MOUSEBUTTONDOWN:
+                    if self.menu.rect.collidepoint(event.pos):
+                        if event.button == 4:
+                            self.menu.scroll(event.pos, False)
+                        elif event.button == 5:
+                            self.menu.scroll(event.pos)
+                    
                 if event.type == pygame.MOUSEMOTION:
                     if self.menu.rect.collidepoint(event.pos):
                         self.menu.mouse_movement(event.pos)
                     else:
                         self.menu.draw_image()
             
-            clock.tick(30)
+            clock.tick(60)
 
 
 x, y = 900, 500
